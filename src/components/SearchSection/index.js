4193a627dd1d0eb9fea1973e46d90608e5f1850e
Samet Yildirim <samety3503@gmail.com>
2023-10-04 16:07:19 +0300

    Movie fav icon functionality add

--- src/components/SearchSection/index.js
+++ src/components/SearchSection/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { HiOutlineSearch } from "react-icons/hi";
+import { AiOutlineStar,AiFillStar } from "react-icons/ai";
 import styles from "./styles.module.scss";
 import cn from "classnames";
 import { ImCross } from "react-icons/im";
@@ -12,6 +13,7 @@ const SearchSection = ({ handleCloseSearch }) => {
   const { baseApiUrl } = enums;
   const [searchText, setSearchText] = useState("");
   const [searchDatas, setSearchDatas] = useState([]);
+  const [favouriteMovies, setFavouriteMovies] = useState(JSON.parse(localStorage.getItem("favouriteMovies")) || [9613]);
 
   const changeSearchText = (event) => setSearchText(event.target.value);
 
@@ -43,6 +45,23 @@ const SearchSection = ({ handleCloseSearch }) => {
 
     return () => clearTimeout(searchTimeout);
   }, [searchText]);
+
+  const handleFavIcon = (id) => {
+    if (favouriteMovies.includes(id)) {
+      const filteredFavMovies = favouriteMovies.filter((movieId) => movieId !== id);
+  
+      setFavouriteMovies(filteredFavMovies);
+  
+      localStorage.setItem("favouriteMovies", JSON.stringify(filteredFavMovies));
+    } else {
+      const newFavMovies = [...favouriteMovies, id];
+  
+      setFavouriteMovies(newFavMovies);
+  
+      localStorage.setItem("favouriteMovies", JSON.stringify(newFavMovies));
+    }
+  }
+
   return (
     <div className={cn("p-1", styles.search)}>
       <div className={cn("w-container mr-auto ml-auto", styles.searchInner)}>
@@ -61,13 +80,24 @@ const SearchSection = ({ handleCloseSearch }) => {
         </div>
         <div className={styles.searchResult}>
           <div className={cn(styles.searchResultWrapper)}>
+            <div className={cn("w-container mr-auto ml-auto", styles.searchResultWrapperInner)}>
             {searchDatas.map((searchData) => (
               <div key={searchData.id} className={styles.searchResultWrapperItemWrapper}>
-                <Link onClick={handleCloseSearch} className="w-container mr-auto ml-auto flex items-center" href={`/movie/${ searchData.id }`}>
+                <Link onClick={handleCloseSearch} className="flex items-center" href={`/movie/${ searchData.id }`}>
                   <HiOutlineSearch className="mr-1" /> {searchData.original_title}
                 </Link>
+                  {
+                    favouriteMovies.includes(searchData.id) ?
+                      <AiFillStar className={cn("ml-1", styles.favIcon, styles.favIconActive)} onClick={()=>{
+                        handleFavIcon(searchData.id)
+                      }} /> :
+                      <AiOutlineStar className={cn("ml-1", styles.favIcon)} onClick={()=>{
+                        handleFavIcon(searchData.id)
+                      }} />
+                  }
               </div>
             ))}
+            </div>
           </div>
         </div>
       </div>
